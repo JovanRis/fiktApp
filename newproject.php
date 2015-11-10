1317277e135c3d196532b67e0265ffb7282c9216
Koding <stefan_krsteski@hotmail.com>
2015-11-10 15:57:54 +0000

    almost done adding project

--- newproject.php
+++ newproject.php
@@ -4,45 +4,68 @@
 
 
    <div id="main">
-       <div class="col-md-4">
-           <div class="btn-group-vertical">
-              <!-- Ako e najaven kako Kompanija da mozi da dodaj  -->
-              <button onclick="location.href='newproject.php'" type="button" class="btn btn-success btn-md">New Project</button>
-              <!-- Ovde ce se iscituvaat proektite so bile napraeni prethodno -->
-              <button type="button" class="btn btn-success btn-md">Coding</button>
-              <button type="button" class="btn btn-success btn-md">Desing</button>
-              <button type="button" class="btn btn-success btn-md">Comunications</button>
-              <button type="button" class="btn btn-success btn-md">Education</button>
-              
-           </div>
-        </div>
-        <div class="col-md-8">
-        <fieldset>
-                <legend>New Project</legend>
-                <ol>
-                <li>
-                 
-                    <input type='radio' name='projecttype' value='coding' checked>Coding
-                    <input type='radio' name='projecttype' value='desing'>Desing
-                    <input type='radio' name='projecttype' value='communication'>Comunications
-                    <input type='radio' name='projecttype' value='education'>Education
+   
+       <?php 
+       if(isset($_POST['submit']))
+       {
+            $companyObj = new Company($user,$pass);
+            
+             $radioCheck = $_POST['projecttype'];
+             $projectname = $_POST['projectname'];
+             $lastname = $_POST['projectdescription'];
+             //$companyID = $_POST['email'];
+             
+            if($userObj-> register($username,$password,$firstname, $lastname, $email))
+             {
+                  echo "Project added successfuly";
+             }
+             else
+             {
+                  echo "Project adding error";
+             }
+        }
+        //else
+        { 
+        echo"<div class='col-md-4'>
+               <div class='btn-group-vertical'>
+                  <!-- Ako e najaven kako Kompanija da mozi da dodaj  -->
+                  <button onclick='location.href='newproject.php'' type='button' class='btn btn-success btn-md'>New Project</button>
+                  <!-- Ovde ce se iscituvaat proektite so bile napraeni prethodno -->
+                  <button type='button' class='btn btn-success btn-md'>Coding</button>
+                  <button type='button' class='btn btn-success btn-md'>Desing</button>
+                  <button type='button' class='btn btn-success btn-md'>Comunications</button>
+                  <button type='button' class='btn btn-success btn-md'>Education</button>
+               </div>
+            </div>
+            <div class='col-md-8'>
+            <fieldset>
+                    <legend>New Project</legend>
+                    <ol>
+                    <li>
+                     
+                        <input type='radio' name='projecttype' value='coding' checked>Coding
+                        <input type='radio' name='projecttype' value='desing'>Desing
+                        <input type='radio' name='projecttype' value='communication'>Comunications
+                        <input type='radio' name='projecttype' value='education'>Education
+                       
+                    </li>
+                    <li>
+                        <label for='firstname'>Project Name:</label> 
+                        <input type='text' class='form-control' name='projectname' value='' id='projectname' />
+                    </li>
+                     <li>
+                        <label for='lastname'>Project Description:</label> 
+                       <textarea rows='4' cols='50' name='projectdescription' id='projectdescription'>Enter Desription of Project here...
+                       </textarea>
+                    </li>
+                     
+                    <input type='submit' class='btn btn-default' name='submit' value='NewProject' />
                    
-                </li>
-                  <li>
-                    <label for='firstname'>Project Name:</label> 
-                    <input type='text' class='form-control' name='projectname' value='' id='projectname' />
-                </li>
-                 <li>
-                    <label for='lastname'>Project Description:</label> 
-                   <textarea rows="4" cols="50" name="projectdescription" id="projectdescription">Enter Desription of Project here...
-                   </textarea>
-                </li>
-                 
-                <input type='submit' class='btn btn-default' name='submit' value='NewProject' />
-               
-        </fieldset>
-        
-        </div>
+            </fieldset>
+            
+            </div>";
+        }
+            ?>
     </div>
 
 </div> <!-- End of outer-wrapper which opens in header.php -->
